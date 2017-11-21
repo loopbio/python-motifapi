86e9139cf7316047b1c40d07bc444f9006ea6c3a
John Stowers <john.stowers@gmail.com>
2017-11-21 18:49:25 +0100

    examples: fix comments

--- examples/test_sched.py
+++ examples/test_sched.py
@@ -25,10 +25,10 @@ print(api.call('schedule'))
 # warning: if you share this recording system with others it will also clear their tasks!
 api.call('schedule/clear')
 
-# schedule a recording for 10 seconds at :32 between the hours of 16-17h, every day
+# schedule a recording for 10 seconds at :09 between the hours of 16-17h, every day
 api.call('schedule/recording/start', task_name='record_video', cron_expression='09 16-17 * * *',
                                      codec='libx264', duration=10)
 
-# at 17:00 schedule all recordings to be copied to the configured location
+# at 17:11 schedule all recordings to be copied to the configured location
 api.call('schedule/camera/%s/recordings/copy_all' % camera_serial, task_name='copy_days_videos', cron_expression='11 17 * * *')
 
