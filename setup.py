a234afd53a59565bbb6e4f6fff16f2e3e2a49c41
John Stowers <john.stowers@gmail.com>
2018-02-14 16:39:32 +0100

    version 0.1.1

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ setup(
     license='BSD',
     description='Python interface to Motif recording systems',
     long_description='Python interface to Motif recording systems',
-    version='0.1.0',
+    version='0.1.1',
     author='John Stowers',
     author_email='john@loopbio.com',
     packages=['motifapi'],
